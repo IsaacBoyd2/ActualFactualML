75c580b20bbd810917425c4501f6c3b088bb7c50
IsaacBoyd2 <97537554+IsaacBoyd2@users.noreply.github.com>
2022-11-24 17:46:58 -0700

    Update mlp.py

--- Project4/code/mlp.py
+++ Project4/code/mlp.py
@@ -17,7 +17,6 @@ import math
 import random as random
 import sys
 
-import loss as lss
 #----------------------classes-------------------------
 
 class Model:
@@ -29,43 +28,72 @@ class Model:
     self.mlp_init = []
     self.values = []
     
-  def run(self, input_size, hidden_sizes, output_size):  
+  def run(self, input_size, hidden_sizes, output_size,flag):  
     #Initilize the network to have random weights between 0 and 1.
 
     mlp_init = []   #The network
 
-    #Input
-    hidden_nodes = []
-    for i in range(input_size):
-      hidden_node = []
-      for i in range(hidden_sizes[0]):
-        hidden_node.append(random.random())
-      hidden_nodes.append(hidden_node)
+    if flag == 0:
 
-    mlp_init.append(hidden_nodes)
-    
-
-    #Init number of weights between each hidden layer
-    for sizes in range(len(hidden_sizes)-1):
+      #Input
       hidden_nodes = []
-      for i in range(hidden_sizes[0+sizes]):
+      for i in range(input_size):
         hidden_node = []
-        for j in range(hidden_sizes[1+sizes]):
+        for i in range(hidden_sizes[0]):
           hidden_node.append(random.random())
         hidden_nodes.append(hidden_node)
 
       mlp_init.append(hidden_nodes)
+      
 
-    #output layer
-    output_nodes = []
-    for i in range(hidden_sizes[-1]):
-      output_node = []
-      for i in range(output_size):
-        output_node.append(random.random())
-      output_nodes.append(output_node)
-    
-    
-    mlp_init.append(output_nodes)
+      
+
+      #Init number of weights between each hidden layer
+      for sizes in range(len(hidden_sizes)-1):
+        hidden_nodes = []
+        for i in range(hidden_sizes[0+sizes]):
+          hidden_node = []
+          for j in range(hidden_sizes[1+sizes]):
+            hidden_node.append(random.random())
+          hidden_nodes.append(hidden_node)
+
+        mlp_init.append(hidden_nodes)
+
+      #output layer
+      output_nodes = []
+      for i in range(hidden_sizes[-1]):
+        output_node = []
+        for i in range(output_size):
+          output_node.append(random.random())
+        output_nodes.append(output_node)
+      
+      
+      mlp_init.append(output_nodes)
+
+    else:
+
+      #Input
+      hidden_nodes = []
+      for i in range(input_size):
+        hidden_node = []
+        for i in range(output_size):
+          hidden_node.append(random.random())
+        hidden_nodes.append(hidden_node)
+
+      mlp_init.append(hidden_nodes)
+      
+
+      #output layer
+      #output_nodes = []
+      #for i in range(input_size):
+      #  output_node = []
+      #  for i in range(output_size):
+      #    output_node.append(random.random())
+      #  output_nodes.append(output_node)
+      
+      
+      #mlp_init.append(output_nodes)
+
 
     #print(mlp_init)
 
@@ -87,52 +115,87 @@ class Model:
 
     '''
     
-  def forwardProp(self,input,classNumber):      #potentially need to do something for just the input layers
+  def forwardProp(self,input, classNumber):
     values = [[]]
-    print('Current Weights:',self.mlp_init,'\n')
     values[0] = input
-    #loops through each layer.
+
+
+    #loops through each layer.  (ex. 0,1,2,3)
     for i in range(len(self.mlp_init)):
       layer_outputs = []
-      #loops through each node      
+ 
       if i != len(self.mlp_init)-1: #As long as we are not in the last layer
-        for j in range(len(self.mlp_init[i+1])):  #This grabs the length of the next layer
+
+        #loops through each node according to the next layer length (ex. 0,1,2,3)  
+        for j in range(len(self.mlp_init[i+1])): 
           l = []
-          for k in range(len(values[i])):   #for every xi
+
+          #This will grab everything in values starting at values[0]
+          for k in range(len(values[i])): 
+            #print(i,k,j)
+            #print('xi*wi : ',l)
             l.append(float(values[i][k])*float(self.mlp_init[i][k][j]))  #do xiwi
           summation = sum(l) #Sum of all xiwis
-          #print("SUMMATION: ", summation)
-          sigmoid = 1/(1+round(math.e**(-summation),8))    #sigmoid function
+          
+          #print('Sum xi*wi : ',summation)
+          
+
+          sigmoid = 1/(1+(math.e**(-summation)))    #sigmoid function
+
+          #print('After sigmoidal activation : ',sigmoid)
           layer_outputs.append(sigmoid) #append for each input
 
+        #print('Entire Layer output : ',layer_outputs)
+
         values.append(layer_outputs) #append all the outputs. (this will be what is "inside" of each node)
-        #print(values)
+
+      
 
       #output layer
-
       elif classNumber == 1:
-        for i in range(1):
-          l = []
-          for k in range(len(values[-1])):   #for every xi
-            l.append(float(values[-1][k])*float(self.mlp_init[-2][k][i]))  #do xiwi
-          summation = sum(l)
-        
+
+        #Linear = No activation
+        l = []
+        for k in range(len(values[-1])):  
+          l.append(float(values[-1][k])*float(self.mlp_init[-1][k][0]))  #do xiwi
+        summation = sum(l)
         output = summation
+
+        layer_outputs = []
+        layer_outputs.append(output)
+        values.append(layer_outputs)
 
         self.output = output
 
-      #Decision Circuit
+        #print('Before linear activation : ', values[-2])
+       
+        #print('Linear Output : ', self.output)
+
+
+        self.values = values
+
+        #print('Output Values for layers 3->1 : ',self.values)
+        #print('Output at layer 0 : ',self.output)
+
+
       else:
         layer_outputs = []
         for i in range(len(self.mlp_init[-1][0])):
+ 
           l = []
           for k in range(len(values[-1])):   #for every xi
             l.append(float(values[-1][k])*float(self.mlp_init[-1][k][i]))  #do xiwi
           summation = sum(l) #Sum of all xiwis
-          sigmoid = 1/(1+math.e**(-summation))    #sigmoid function
-          layer_outputs.append(sigmoid) #append for each input
 
-        values.append(layer_outputs) #append all the outputs. (this will be what is "inside" of each node)
+
+          sigmoid = 1/(1+math.e**(-summation))    #sigmoid function
+
+
+          layer_outputs.append(sigmoid) 
+
+        values.append(layer_outputs)
+
+        
       
       if classNumber == 0:
 
@@ -149,15 +212,101 @@ class Model:
           softmax2 = (math.e**i)/the_sum_of_soft
           output_values.append(softmax2)
 
+        #print('weights', self.mlp_init[-1])
+        #print("Before Softamx : " ,values[-1])
+
         values[-1] = output_values
+
+        #print('After Softmax : ',values[-1])
 
       
     self.values = values
+    
 
+  def Back_Prop(self,eta,classNumber,actual,output_size):  
+    
+    #Preapre Deltas for delta rule
+    deltas=[]  
+    for x in range(len(self.values)):
+      deltas.append([])
+
+    counter = 0
+    #go through every layer backwards (ex. 3,2,1,0)
+    for i in reversed(range(len(self.values))):   #we need #of deltas = # of layers so len delta = len values.... Used to be len deltas = len self.mlp_intit
+
+      #farthest_layer_right = self.mlp_init[0]
+
+      if i == len(self.values)- 1:    #output layer
+        if classNumber == 1:
+          diff = actual - self.output    #(t - a)    t = actual a = guess
+          deltas[counter].append(diff)   
+          
+
+        else:
+          actualClass = actual[1]
+          actualOneHot = actual[0]
+
+          for j in range(len(self.values[i])):
+            diff = actualOneHot.get(actualClass)[j] - self.values[i][j]
+            deltas[0].append(diff)
+
+      else:
+        #print(counter)
+
+        #Go through every node in the current Layer and assign each one a delta
+        for j in range(len(self.values[i])): 
+
+          xi = self.values[i][j]
+          sumwih_deltai = 0
+
+          
+
+          #This grabs how many nodes are in the layer ahead. That is how many components will be in each delta calc. (ex. 0,1)
+          for l in range(len(deltas[counter-1])):
+
+            deltai = deltas[counter-1][l]
+            weight_s = self.mlp_init[i][j][l]  
+
+            #get sum(wiDi)
+            sumwih_deltai += weight_s*deltai
+          
+          #Get delta for the given node.
+          delcalc = sumwih_deltai*(xi)*(1-xi) 
+          deltas[counter].append(delcalc)
+ 
+
+
+
+
+    
+
+      counter = counter + 1  
+
+    #print('Before weights update : ',self.mlp_init)
+
+    deltas.reverse()
+    #print(deltas)
+
+    #Now we need to update the weights
+    #go through every layer
+    for i in range(len(self.mlp_init)):
+      layer = self.mlp_init[i]
+      #go through every node in every layer
+      for j in range(len(layer)):
+        neuron = layer[j]
+        #go through every weight
+        for k in range(len(neuron)):
+          #print(self.mlp_init[i][j][k])
+          #print(self.values[i][j])
+
+          #should be every weight  + eta*delta in from of the weight*xi that caused the weight
+          self.mlp_init[i][j][k] = self.mlp_init[i][j][k] + eta*deltas[i+1][k]*self.values[i][j]    #delta needs to be +1 so we do not pull from the input layer
+
+    #print('After weights update : ',self.mlp_init)
+    
   def change_mlp_init(self, change):
     self.mlp_init = change
-
-
+    
   def algGA(self):
     pass
 
@@ -183,5 +332,3 @@ class Model:
         if i == 0 and j == 0:
           for k in range(numPart):
             parts[k].forwardProp(#FINISH THIS)
-
-  
