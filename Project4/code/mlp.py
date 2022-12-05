f658aa9714794b98bc46990d8a58d63dbd835951
JamesLucas943 <56455079+JamesLucas943@users.noreply.github.com>
2022-12-04 18:07:38 -0700

    Update mlp.py

--- Project4/code/mlp.py
+++ Project4/code/mlp.py
@@ -167,15 +167,18 @@ class Model:
   def algPSO(self, particles, dataType, training_df, testing_df, testing_df_with_labels):
     #----Hyper-Parameters----
 
-    c_1 = 1.496
-    c_2 = 1.496
+    # c_1 = 1.496
+    # c_2 = 1.496
+
+    c_1 = 2.5
+    c_2 = 2.5
 
     #----Variables----
     parts = particles
     uniques = training_df['Class'].unique()
     r_1 = 0
     r_2 = 0
-    loops = 1000
+    loops = 100
     pb = []
 
     #used to count all of the weights in the model objects (used for velocity)
