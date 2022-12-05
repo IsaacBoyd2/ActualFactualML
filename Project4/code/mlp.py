a46415548457c96f3b0bee06ea0e688f67b600a3
JamesLucas943 <56455079+JamesLucas943@users.noreply.github.com>
2022-12-04 17:09:12 -0700

    Update mlp.py

--- Project4/code/mlp.py
+++ Project4/code/mlp.py
@@ -166,11 +166,9 @@ class Model:
 
   def algPSO(self, particles, dataType, training_df, testing_df, testing_df_with_labels):
     #----Hyper-Parameters----
-    # c_1 = 1.496
-    # c_2 = 1.496
 
-    c_1 = .1
-    c_2 = .1
+    c_1 = 1.496
+    c_2 = 1.496
 
     #----Variables----
     parts = particles
